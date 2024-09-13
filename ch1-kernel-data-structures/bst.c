a7df97652ea89a1ff2bf80b783bf6aad24d180f4
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-09-14 01:12:53 +0500

    Simple BST implementation

--- ch1-kernel-data-structures/bst.c
+++ ch1-kernel-data-structures/bst.c
@@ -43,6 +43,21 @@ void inOrderTraversal(Node* root){
     }
 }
 
+void preOrderTraversal(Node* root){
+    if (root != NULL){
+        printf("%d ", root->data);
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
+
+void postOrderTraversal(Node* root){
+    if (root != NULL){
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
 
 
 
@@ -50,12 +65,24 @@ int main()
 {
     // Soon, BST implementation will be written
     Node* root = NULL;
-    root = insert(root, 17);
+    root = insert(root, 47);
     insert(root, 30);
     insert(root, 20);
+    insert(root, 33);
+    insert(root, 54);
+    insert(root, 51);
+    insert(root, 52);
 
     printf("Inorder traversal of the BST: ");
     inOrderTraversal(root);
+
+    printf("\n\n");
+    printf("PreOrder traversal of the BST: ");
+    preOrderTraversal(root);
+
+    printf("\n\n");
+    printf("PostOrder traversal of the BST");
+    postOrderTraversal(root);
 
     return 0;
 }
